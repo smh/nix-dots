d9e0297fd6633755cbc7c9a23ce3d992bf97a581
Stein Martin Hustad <stein@hustad.com>
2024-12-03 21:30:38 +0400

    Updated homelab

--- modules/homelab/default.nix
+++ modules/homelab/default.nix
@@ -6,21 +6,11 @@ in {
   options.services.homelab = with lib; {
     enable = mkEnableOption "homelab services";
     
-    mediaPath = mkOption {
-      type = types.str;
-      default = "/Volumes/Media";
-      description = "Base path for media storage";
-    };
-
-    nfsServer = mkOption {
-      type = types.str;
-      description = "NFS server hostname or IP";
-    };
-
-    nfsShare = mkOption {
-      type = types.str;
-      description = "NFS share path on server";
-    };
+    # mediaPath = mkOption {
+    #   type = types.str;
+    #   default = "/data/media";
+    #   description = "Base path for media storage";
+    # };
   };
 
   config = lib.mkIf cfg.enable {
@@ -33,33 +23,97 @@ in {
       prowlarr
       sabnzbd
       nzbhydra2
-      
+
       # Support tools
       ffmpeg
       mediainfo
     ];
 
-    # NFS automount setup for macOS
-    system.activationScripts.automount-media = ''
-      # Add our media mount to auto_master if not already present
-      if ! grep -q '${cfg.mediaPath}' /etc/auto_master; then
-        echo '${cfg.mediaPath}    auto_media    -nobrowse,hidefromfinder' | sudo tee -a /etc/auto_master
-      fi
-    '';
-
-    environment.etc."auto_media" = {
-      text = ''
-        * -fstype=nfs,soft,timeo=15,retrans=2,rw ${cfg.nfsServer}:${cfg.nfsShare}
-      '';
+    # NFS client support
+    services.rpcbind.enable = true;
+    
+    # NFS mount configuration
+    fileSystems = {
+      "/data" = {
+        device = "blackhole.lan:/data";
+        fsType = "nfs";
+        options = [
+          "nofail"          # Don't fail boot if mount fails
+          "soft"            # Return errors rather than hang
+          "timeo=15"        # Timeout after 15 seconds
+          "retrans=2"       # Number of retries before failure
+          "rw"             # Mount read-write
+          "x-systemd.automount"  # Automount on access
+          "x-systemd.idle-timeout=600"  # Unmount after 10 minutes of inactivity
+        ];
+      };
     };
 
-    # For now, we'll only enable Plex as it's the only service with native darwin support
-    # services.plex = {
-    #   enable = true;
-    #   openFirewall = true;
+    # Enable and configure media services
+    services = {
+      plex = {
+        enable = true;
+        openFirewall = true;
+        user = "plex";
+        group = "media";
+        dataDir = "/var/lib/plex";
+      };
+
+      sonarr = {
+        enable = true;
+        user = "sonarr";
+        group = "media";
+        dataDir = "/var/lib/sonarr";
+      };
+
+      radarr = {
+        enable = true;
+        user = "radarr";
+        group = "media";
+        dataDir = "/var/lib/radarr";
+      };
+
+      prowlarr = {
+        enable = true;
+      };
+
+      jellyseerr = {
+        enable = true;
+      };
+    };
+
+    # Open required ports in firewall
+    networking.firewall = {
+      allowedTCPPorts = [
+        32400  # Plex
+        8989   # Sonarr
+        7878   # Radarr
+        9696   # Prowlarr
+        8080   # SABnzbd
+        5076   # NZBHydra2
+      ];
+    };
+
+    # Create shared media group and add service users to it
+    users.groups.media = {
+      gid = 2000;
+    };
+
+    # users.users.media = {
+    #   uid = 2000;
+    #   group = "media";
     # };
 
-    # TODO: For other services, we'll need to set up launchd services manually
-    # or use alternatives like running them in containers
+    # users.users = {
+    #   plex.extraGroups = [ "media" ];
+    #   sonarr.extraGroups = [ "media" ];
+    #   radarr.extraGroups = [ "media" ];
+    #   # prowlarr.extraGroups = [ "media" ];
+    # };
+
+    # Ensure media directory has correct permissions
+    systemd.tmpfiles.rules = [
+      "d /data 0775 root media - -"
+    ];
   };
 }
