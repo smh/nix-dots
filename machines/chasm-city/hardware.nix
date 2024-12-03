283c36345c30eef737678b7240a3f63ad575bfe3
Stein Martin Hustad <stein@hustad.com>
2024-12-03 23:45:51 +0400

    Attempt building vmware image

--- machines/chasm-city/hardware.nix
+++ machines/chasm-city/hardware.nix
@@ -4,23 +4,30 @@
   # boot.loader.grub.enable = false;
   # hardware.enableAllFirmware = false;
 
+  # Interface is this on M1
+  # networking.interfaces.ens160.useDHCP = true;
+
+  # Lots of stuff that uses aarch64 that claims doesn't work, but actually works.
+  # nixpkgs.config.allowUnfree = true;
+  # nixpkgs.config.allowUnsupportedSystem = true;
+
   networking = {
-    dhcpcd.enable = false;
-    useDHCP = false;
+    # dhcpcd.enable = false;
+    # useDHCP = false;
     useHostResolvConf = false;
   };
 
-  systemd.network = {
-    enable = true;
-    networks."50-eth0" = {
-      matchConfig.Name = "eth0";
-      networkConfig = {
-        DHCP = "ipv4";
-        IPv6AcceptRA = true;
-      };
-      linkConfig.RequiredForOnline = "routable";
-    };
-  };
+  # systemd.network = {
+  #   enable = true;
+  #   networks."50-eth0" = {
+  #     matchConfig.Name = "eth0";
+  #     networkConfig = {
+  #       DHCP = "ipv4";
+  #       IPv6AcceptRA = true;
+  #     };
+  #     linkConfig.RequiredForOnline = "routable";
+  #   };
+  # };
 
   # systemd.suppressedSystemUnits = [
   #   "dev-mqueue.mount"
