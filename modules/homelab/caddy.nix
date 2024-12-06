fbbc658d82341a856f7dbf5f7fb059157825deb2
Stein Martin Hustad <stein@hustad.com>
2024-12-06 08:21:48 +0400

    Update caddy with certs from certbot

--- modules/homelab/caddy.nix
+++ modules/homelab/caddy.nix
@@ -2,96 +2,55 @@
 
 let
   domain = "hl.hustad.dev";
+  certDir = "/var/lib/acme/certs";
+  mkVirtualHost = port: extraHeaders: {
+    extraConfig = ''
+      tls ${certDir}/fullchain.pem ${certDir}/privkey.pem
+      reverse_proxy :${toString port} {
+        header_up Host {host}
+        ${extraHeaders}
+      }
+    '';
+  };
 in {
   services.caddy = {
     enable = true;
-    
+
+    # The global config should not be nested
+    globalConfig = ''
+      default_sni ${domain}
+      auto_https off
+    '';
+
     virtualHosts = {
-      "bazarr.${domain}" = {
-        extraConfig = ''
-          reverse_proxy :6768 {
-            header_up Host {host}
-          }
-        '';
-      };
-
-      #nzbhydra2
-      "hydra.${domain}" = {
-        extraConfig = ''
-          reverse_proxy :5075 {
-            header_up Host {host}
-          }
-        '';
-      };
-
-      "lidarr.${domain}" = {
-        extraConfig = ''
-          reverse_proxy :8787 {
-            header_up Host {host}
-          }
-        '';
-      };
-
-      "plex.${domain}" = {
-        extraConfig = ''
-          reverse_proxy :32400 {
-            header_up Host {host}
-            header_up X-Forwarded-Proto https
-          }
-        '';
-      };
-
-      "prowlarr.${domain}" = {
-        extraConfig = ''
-          reverse_proxy :9697 {
-            header_up Host {host}
-          }
-        '';
-      };
-
-      "radarr.${domain}" = {
-        extraConfig = ''
-          reverse_proxy :7879 {
-            header_up Host {host}
-          }
-        '';
-      };
-
-      "readarr.${domain}" = {
-        extraConfig = ''
-          reverse_proxy :8788 {
-            header_up Host {host}
-          }
-        '';
-      };
-
-      # Jellyseerr
-      "request.${domain}" = {
-        extraConfig = ''
-          reverse_proxy :5055 {
-            header_up Host {host}
-          }
-        '';
-      };
-
-      # Sabnzbd
-      "sab.${domain}" = {
-        extraConfig = ''
-          reverse_proxy :9090 {
-            header_up Host {host}
-          }
-        '';
-      };
-
-      "sonarr.${domain}" = {
-        extraConfig = ''
-          reverse_proxy :9898 {
-            header_up Host {host}
-          }
-        '';
-      };
+      "bazarr.${domain}" = mkVirtualHost 6767 "";
+      "hydra.${domain}" = mkVirtualHost 5076 "";  # nzbhydra2
+      "lidarr.${domain}" = mkVirtualHost 8686 "";
+      "plex.${domain}" = mkVirtualHost 32400 "header_up X-Forwarded-Proto https"; # Fixed double header_up
+      "prowlarr.${domain}" = mkVirtualHost 9696 "";
+      "radarr.${domain}" = mkVirtualHost 7878 "";
+      "readarr.${domain}" = mkVirtualHost 8787 "";
+      "request.${domain}" = mkVirtualHost 5055 ""; # Jellyseerr
+      "sab.${domain}" = mkVirtualHost 8080 ""; # Sabnzbd
+      "sonarr.${domain}" = mkVirtualHost 8989 "";
     };
   };
+
+
+  systemd.services.caddy.serviceConfig = {
+    # SupplementaryGroups = [ "certbot" ]; handled by certbot.nix
+    # AssertPathExists = [
+    #   "${certDir}/fullchain.pem"
+    #   "${certDir}/privkey.pem"
+    # ];
+    unitConfig = {
+      ConditionPathExists = [
+        "${certDir}/fullchain.pem"
+        "${certDir}/privkey.pem"
+      ];
+    };
+  };
+  # systemd.services.caddy.serviceConfig.SupplementaryGroups = [ "certbot" ];
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
