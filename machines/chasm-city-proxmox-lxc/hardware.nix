f9de78fff0f26c49bd624315a31d0d16f4a48bea
Stein Martin Hustad <stein@hustad.com>
2024-12-11 13:23:32 +0400

    wip

--- machines/chasm-city-proxmox-lxc/hardware.nix
+++ machines/chasm-city-proxmox-lxc/hardware.nix
@@ -1,7 +1,7 @@
 { inputs, outputs, ... }: {
 
-  # boot.isContainer = true;
-  # boot.loader.grub.enable = false;
+  boot.isContainer = true;
+  boot.loader.grub.enable = false;
   # hardware.enableAllFirmware = false;
 
   # Interface is this on M1
@@ -17,23 +17,23 @@
     useHostResolvConf = false;
   };
 
-  # systemd.network = {
-  #   enable = true;
-  #   networks."50-eth0" = {
-  #     matchConfig.Name = "eth0";
-  #     networkConfig = {
-  #       DHCP = "ipv4";
-  #       IPv6AcceptRA = true;
-  #     };
-  #     linkConfig.RequiredForOnline = "routable";
-  #   };
-  # };
+  systemd.network = {
+    enable = true;
+    networks."50-eth0" = {
+      matchConfig.Name = "eth0";
+      networkConfig = {
+        DHCP = "ipv4";
+        IPv6AcceptRA = true;
+      };
+      linkConfig.RequiredForOnline = "routable";
+    };
+  };
 
-  # systemd.suppressedSystemUnits = [
-  #   "dev-mqueue.mount"
-  #   "sys-kernel-debug.mount"
-  #   "sys-fs-fuse-connections.mount"
-  # ];
+  systemd.suppressedSystemUnits = [
+    "dev-mqueue.mount"
+    "sys-kernel-debug.mount"
+    "sys-fs-fuse-connections.mount"
+  ];
 
   # Enable udev
   # services.udev = {
