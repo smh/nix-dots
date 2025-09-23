42971bffb4f061a049116fae9223e8ed9d50118c
Stein Martin Hustad <stein@hustad.com>
2025-09-23 14:51:25 +0400

    align tmux catppuccin theme with that of ghostty/vim

--- home/smh/programs/tmux/default.nix
+++ home/smh/programs/tmux/default.nix
@@ -81,7 +81,7 @@
         };
         extraConfig = ''
           # Configure Catppuccin
-          set -g @catppuccin_flavor "macchiato"
+          set -g @catppuccin_flavor "mocha"
           set -g @catppuccin_status_background "none"
           set -g @catppuccin_window_status_style "none"
           set -g @catppuccin_pane_status_enabled "off"
