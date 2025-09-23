56730908dde42a29062aef87d03cfac9e93f8d3d
Stein Martin Hustad <stein@hustad.com>
2025-09-23 16:04:23 +0400

    Fixed tmux plugin load order

--- home/smh/programs/tmux/default.nix
+++ home/smh/programs/tmux/default.nix
@@ -28,25 +28,10 @@
         extraConfig = "set -g @fzf-url-fzf-options '-w 50% -h 50% --multi -0 --no-preview --no-border'";
       }
       {
-        plugin = online-status;
-        extraConfig = ''
-          set -g @online_icon "ok"
-          set -g @offline_icon "nok"
-        '';
-      }
-      {
         plugin = tmux-thumbs;
         extraConfig = ''
           set -g @thumbs-key F
         '';
-      }
-      {
-        plugin = mkTmuxPlugin {
-          pluginName = "tmux-battery";
-          src = inputs.tmux-battery;
-          rtpFilePath = "battery.tmux";
-          version = "git";
-        };
       }
       {
         plugin = mkTmuxPlugin {
@@ -110,7 +95,49 @@
           set -ga status-right "#[bg=#{@thm_bg}]#{?#{==:#{online_status},ok},#[fg=#{@thm_mauve}] 󰖩 on ,#[fg=#{@thm_red},bold]#[reverse] 󰖪 off }"
           set -ga status-right "#[bg=#{@thm_bg},fg=#{@thm_overlay_0}, none]│"
           set -ga status-right "#[bg=#{@thm_bg},fg=#{@thm_blue}] 󰭦 %Y-%m-%d 󰅐 %H:%M "
+
+          ##### catppuccin theme post plugin config ######
+          set -g status-position top
+          set -g status-style "bg=#{@thm_bg}"
+          set -g status-justify "absolute-centre"
+
+          # pane border look and feel
+          setw -g pane-border-status top
+          setw -g pane-border-format ""
+          setw -g pane-active-border-style "bg=#{@thm_bg},fg=#{@thm_overlay_0}"
+          setw -g pane-border-style "bg=#{@thm_bg},fg=#{@thm_surface_0}"
+          setw -g pane-border-lines single
+
+          # window look and feel
+          set -wg automatic-rename on
+          set -g automatic-rename-format "Window"
+
+          set -g window-status-format " #I#{?#{!=:#{window_name},Window},: #W,} "
+          set -g window-status-style "bg=#{@thm_bg},fg=#{@thm_rosewater}"
+          set -g window-status-last-style "bg=#{@thm_bg},fg=#{@thm_peach}"
+          set -g window-status-activity-style "bg=#{@thm_red},fg=#{@thm_bg}"
+          set -g window-status-bell-style "bg=#{@thm_red},fg=#{@thm_bg},bold"
+          set -gF window-status-separator "#[bg=#{@thm_bg},fg=#{@thm_overlay_0}]│"
+
+          set -g window-status-current-format " #I#{?#{!=:#{window_name},Window},: #W,} "
+          set -g window-status-current-style "bg=#{@thm_peach},fg=#{@thm_bg},bold"
+          ##### end catppuccin theme post plugin config ######
         '';
+      }
+      {
+        plugin = online-status;
+        extraConfig = ''
+          set -g @online_icon "ok"
+          set -g @offline_icon "nok"
+        '';
+      }
+      {
+        plugin = mkTmuxPlugin {
+          pluginName = "tmux-battery";
+          src = inputs.tmux-battery;
+          rtpFilePath = "battery.tmux";
+          version = "git";
+        };
       }
     ];
 
@@ -149,33 +176,6 @@
 
       # Allow color passthrough
       set -g allow-passthrough on
-
-      ##### catppuccin theme post plugin config ######
-      set -g status-position top
-      set -g status-style "bg=#{@thm_bg}"
-      set -g status-justify "absolute-centre"
-
-      # pane border look and feel
-      setw -g pane-border-status top
-      setw -g pane-border-format ""
-      setw -g pane-active-border-style "bg=#{@thm_bg},fg=#{@thm_overlay_0}"
-      setw -g pane-border-style "bg=#{@thm_bg},fg=#{@thm_surface_0}"
-      setw -g pane-border-lines single
-
-      # window look and feel
-      set -wg automatic-rename on
-      set -g automatic-rename-format "Window"
-
-      set -g window-status-format " #I#{?#{!=:#{window_name},Window},: #W,} "
-      set -g window-status-style "bg=#{@thm_bg},fg=#{@thm_rosewater}"
-      set -g window-status-last-style "bg=#{@thm_bg},fg=#{@thm_peach}"
-      set -g window-status-activity-style "bg=#{@thm_red},fg=#{@thm_bg}"
-      set -g window-status-bell-style "bg=#{@thm_red},fg=#{@thm_bg},bold"
-      set -gF window-status-separator "#[bg=#{@thm_bg},fg=#{@thm_overlay_0}]│"
-
-      set -g window-status-current-format " #I#{?#{!=:#{window_name},Window},: #W,} "
-      set -g window-status-current-style "bg=#{@thm_peach},fg=#{@thm_bg},bold"
-      ##### end catppuccin theme post plugin config ######
     '';
   };
 }
